62dadc9b7de8b982b44ea98aebea6345926c97b5
Ryan Hoegg <ryan.hoegg@gmail.com>
2017-07-08 18:52:30 -0500

    Spike to get mule configuration files into the bundle with prefixes

--- src/main/java/software/hoegg/mule/ApplicationBundleMojo.java
+++ src/main/java/software/hoegg/mule/ApplicationBundleMojo.java
@@ -9,11 +9,15 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.components.io.fileselectors.FileSelector;
+import org.codehaus.plexus.components.io.fileselectors.IncludeExcludeFileSelector;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.File;
+import java.util.*;
 
 /**
  * Creates a single mule application by bundling together the applications in the dependencies
@@ -24,9 +28,40 @@ public class ApplicationBundleMojo extends AbstractMojo {
 	@Component
 	protected MavenProject mavenProject;
 
+	@Component
+	protected TransformZipUnArchiver unArchiver;
+
+	@Parameter(defaultValue = "${project.build.directory}/mule-bundle", required = true)
+	protected File outputDirectory;
+
 	public void execute() throws MojoExecutionException, MojoFailureException {
 
-		getLog().info("Should bundle " + StringUtils.join(getZipDependencies(), ","));
+		getLog().debug("Should bundle " + StringUtils.join(getZipDependencies(), ","));
+		outputDirectory.mkdirs();
+
+		unArchiver.setDestDirectory(outputDirectory);
+		unArchiver.setFileSelectors(new FileSelector[] {
+			muleConfigSelector()
+		});
+
+		final PrefixMuleConfigTransformer transformer = new PrefixMuleConfigTransformer();
+		unArchiver.setTransformer(transformer);
+
+		for (Artifact appArtifact : getZipDependencies()) {
+			unArchiver.setSourceFile(appArtifact.getFile());
+			transformer.setPrefix(appArtifact.getArtifactId() + ".");
+			unArchiver.extract();
+		}
+
+		getLog().info("Bundled mule configurations: " + StringUtils.join(transformer.getIncludedFiles(), ","));
+	}
+
+	private IncludeExcludeFileSelector muleConfigSelector() {
+		IncludeExcludeFileSelector s = new IncludeExcludeFileSelector();
+		s.setIncludes(new String[] {
+			"*.xml"
+		});
+		return s;
 	}
 
 	private Set<Artifact> getZipDependencies() {
@@ -35,5 +70,36 @@ public class ApplicationBundleMojo extends AbstractMojo {
 				return "zip".equals(a.getType());
 			}
 		}, new HashSet<Artifact>());
+	}
+
+	public class PrefixMuleConfigTransformer implements TransformZipUnArchiver.Transformer {
+		private String prefix;
+		private List<String> includedFiles = new ArrayList<String>();
+
+		public File transformDestinationDirectory(TransformZipUnArchiver.EntryContext context) {
+			return context.getDestinationDirectory();
+		}
+
+		public String transformEntryName(TransformZipUnArchiver.EntryContext context) {
+			String name = getPrefix() + context.getEntryName();
+			includedFiles.add(name);
+			return name;
+		}
+
+		public Date transformEntryDate(TransformZipUnArchiver.EntryContext context) {
+			return context.getEntryDate();
+		}
+
+		public List<String> getIncludedFiles() {
+			return Collections.unmodifiableList(includedFiles);
+		}
+
+		public String getPrefix() {
+			return prefix;
+		}
+
+		public void setPrefix(String prefix) {
+			this.prefix = prefix;
+		}
 	}
 }
---
+++ src/main/java/software/hoegg/mule/TransformZipUnArchiver.java
@@ -0,0 +1,94 @@
+package software.hoegg.mule;
+
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class TransformZipUnArchiver extends ZipUnArchiver {
+	private Transformer transformer = new Transformer() {
+
+		public File transformDestinationDirectory(EntryContext context) {
+			return context.getDestinationDirectory();
+		}
+
+		public String transformEntryName(EntryContext context) {
+			return context.getEntryName();
+		}
+
+		public Date transformEntryDate(EntryContext context) {
+			return context.getEntryDate();
+		}
+	};
+
+	@Override
+	protected void extractFile(File sourceFile, File destDirectory, InputStream compressedInputStream, String entryName, Date entryDate, boolean isDirectory, Integer mode) throws IOException, ArchiverException {
+		final EntryContext context = new EntryContext(sourceFile, destDirectory, entryName, entryDate, isDirectory, mode);
+		super.extractFile(
+			sourceFile,
+			transformer.transformDestinationDirectory(context),
+			compressedInputStream,
+			transformer.transformEntryName(context),
+			transformer.transformEntryDate(context),
+			isDirectory,
+			mode);
+	}
+
+	public void setTransformer(Transformer transformer) {
+		this.transformer = transformer;
+	}
+
+	public interface Transformer {
+		File transformDestinationDirectory(EntryContext context);
+		String transformEntryName(EntryContext context);
+		Date transformEntryDate(EntryContext context);
+	}
+
+	public static class EntryContext {
+		private final File sourceFile;
+		private final File destinationDirectory;
+		private final String entryName;
+		private final Date entryDate;
+		private final boolean isDirectory;
+		private final Integer mode;
+
+		public EntryContext(File sourceFile, File destDirectory, String entryName, Date entryDate, boolean isDirectory, Integer mode) {
+			this.sourceFile = sourceFile;
+			this.destinationDirectory = destDirectory;
+			this.entryName = entryName;
+			this.entryDate = entryDate;
+			this.isDirectory = isDirectory;
+			this.mode = mode;
+		}
+
+		public File getSourceFile() {
+			return sourceFile;
+		}
+
+		public File getDestinationDirectory() {
+			return destinationDirectory;
+		}
+
+		public String getEntryName() {
+			return entryName;
+		}
+
+		public Date getEntryDate() {
+			return entryDate;
+		}
+
+		public boolean isDirectory() {
+			return isDirectory;
+		}
+
+		public Integer getMode() {
+			return mode;
+		}
+	}
+}
